b092225a0c9dc1adec06ef566c527afdcd2352a8
Ronin <saadmalik12148@gmail.com>
2026-07-18 09:00:39 +0500

    feat(migration): remove migrated lab modules from core framework

--- vireon/plugins/ble/__init__.py
+++
@@ -1,1 +0,0 @@
-# VIREON BLE Plugin Package
--- vireon/plugins/firmware/__init__.py
+++
@@ -1,4 +0,0 @@
-from .cortex_m_stub import CortexMStub, FirmwareSecurityMonitor
-from .qemu_hil import QemuCortexMEmulator
-
-__all__ = ["CortexMStub", "FirmwareSecurityMonitor", "QemuCortexMEmulator"]
